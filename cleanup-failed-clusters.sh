247ccffd1c6b6c64347738eef2b8febc70ad10ed
Clarence Bakirtzidis <clarenceb@gmail.com>
2021-11-29 17:34:37 +1100

    Fixed cleanup failed clusters script

--- cleanup-failed-clusters.sh
+++ cleanup-failed-clusters.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+#
+# The script can be run to clean up ARO clusters that are in a failed state.
 
 aroRpObjectId="$(az ad sp list --filter "displayname eq 'Azure Red Hat OpenShift RP'" --query "[?appDisplayName=='Azure Red Hat OpenShift RP'].objectId" -o tsv)"
 
-az role assignment list --assignee $aroRpObjectId
+echo "The Azure Red Hat OpenShift RP ($aroRpObjectId) currently the following remaining role assignments:"
+az role assignment list --assignee $aroRpObjectId -o table
 
 failedClusterResourceGroups="$(az aro list --query "[?provisioningState=='Failed'].{resourceGroup:clusterProfile.resourceGroupId}" -o tsv)"
 
 for nodeResourceGroup in $failedClusterResourceGroups; do
-  echo az role assignment create --assignee $aroRpObjectId --role "User Access Administrator" --scope $nodeResourceGroup
+  echo "Granting 'User Access Administrator' role to Azure Red Hat OpenShift RP ($aroRpObjectId) on scope $nodeResourceGroup"
+  az role assignment create --assignee $aroRpObjectId --role "User Access Administrator" --scope $nodeResourceGroup
 done
 
+echo "Cleaning up failed ARO clusters..."
 az aro list --query "[?provisioningState=='Failed'].{name:name, resourceGroup:resourceGroup}" -o tsv \
     | awk 'BEGIN { FS = "\t" } { print "az aro delete --yes -n " $1 " -g " $2 }' \
     | xargs -I {} bash -c '{}'
+
+echo "The Azure Red Hat OpenShift RP ($aroRpObjectId) now has the following remaining role assignments:"
+az role assignment list --assignee $aroRpObjectId -o table
